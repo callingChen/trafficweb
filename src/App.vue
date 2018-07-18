f2b81726c0be3881333eca142c58ddfae041a4dc
callingChen <zpc18229876889@163.com>
2018-07-18 12:25:38 +0800

    提交工程

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <LoginForm></LoginForm>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm'
+export default {
+  name: 'App',
+  components: {
+    'LoginForm': LoginForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
